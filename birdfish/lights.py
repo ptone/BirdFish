cd444318c81872c5a5cab0a91cb203b3a4142c63
Preston Holmes <preston@ptone.com>
2013-01-24 05:59:44 -0800

    time delta on show now, not on self

--- birdfish/lights.py
+++ birdfish/lights.py
@@ -390,7 +390,7 @@ class Chase(LightGroup):
             # this min max business is because the tween algos will overshoot
             # TODO there is a glitch in the pulse demo where it struggles to
             # get to the very end
-            new_position = self.move_envelope.update(self.show.time_delta)
+            new_position = self.move_envelope.update(show.time_delta)
             if self.moveto > self.center_position:
                 self.center_position = min(self.moveto, new_position)
             else:
