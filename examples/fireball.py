8ddb7a993b57c2b1db73868b37bfeb108fccdbb4
Preston Holmes <preston@ptone.com>
2013-02-18 09:47:06 -0800

    annotate fireball effect

--- examples/fireball.py
+++ examples/fireball.py
@@ -30,29 +30,34 @@ dispatcher = MidiDispatcher("MidiKeys")
 single = RGBLight(
         start_channel=61,
         name="singletestb",
-        attack_duration=0,
-        decay_duration=0,
-        release_duration=0,
-        sustain_value=1,
         )
-single.hue = colors.RED
-single.saturation = 1
-single.update_rgb()
 
 # Define the fireball effect
 # this consists of a colorshift that has changes to hue, saturation and intensity
+# the use of the OUT_CIRC shape is used to make most of the change happen
+# right away, then slowly complete the rest
+# the add shift take a start, end, duration, and shape
 fireball_duration = 2
 fireball = ColorShift()
+# we don't want the effect to loop back, so we disable the default looping
 fireball.set_loop(None)
+# change from yello to red over the duration of the effect
 fireball.add_hue_shift(.13, 0, fireball_duration, shape=tween.OUT_CIRC)
+# at the beginning, start as white, and in the first 1/8th of the effect
+# fade from white to full current hue
 fireball.add_saturation_shift(0, 1, fireball_duration/8, shape=tween.OUT_CIRC)
+# keep full intensity over the first third of the effect
 fireball.add_intensity_shift(1, 1, fireball_duration/3)
+# then for the next two thirds, fade out
 fireball.add_intensity_shift(1, 0, (fireball_duration/3) * 2, shape=tween.OUT_CIRC)
 
+# add the effect to our single test element
 single.effects.append(fireball)
 
 show.add_element(single, network=dmx3)
 
+# now we create a chase of elements, and apply the effect to each element
+# the result is that the effect is painted over time
 chase = Chase(name='fireball streak')
 
 elementid = 0
@@ -61,25 +66,18 @@ for i in range(1, 300, 3):
     l = RGBLight(
             start_channel=i,
             name="item_%s" % elementid,
-            attack_duration=0,
-            decay_duration=0,
-            release_duration=0,
-            sustain_value=1,
             )
     dmx3.add_element(l)
     chase.elements.append(l)
     # deepcopy is used so that each element has an independent fireball effect
-    # over time
+    # over time, if they shared one copy of the effect, then the fade would
+    # be synchronized, which is not what we want
     fb = deepcopy(fireball)
     l.effects.append(fb)
-    # l.effects.append(fireball)
-
 
 chase.end_pos = 70
-print chase.moveto
 chase.speed = 3
-print chase.speed
-# add the light to a network
+
 show.add_element(chase)
 
 # set the input interface to trigger the element
